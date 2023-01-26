73151a55d4f2be7e14e572ff2a47077d4ea504fb
RyzhakovAV <rjizhakov@mail.ru>
2023-01-26 10:35:14 +0500

    reverse

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.add(7);
+        list.add(9);
+        list.add(567);
+        list.add(45);
+        list.add(103);
+        list.add(21);
+        list.printSinglyLinkedList(list);
+        list.reverse();
+        list.printSinglyLinkedList(list);
     }
 }
